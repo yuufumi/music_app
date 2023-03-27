0400ecc43cb26bcf5507952615f15a749a2ef42c
Youcef BENALI <105224275+yuufumi@users.noreply.github.com>
2023-03-27 23:22:03 +0100

    all songs page added + fetching local storage mp

--- musically/lib/main.dart
+++ musically/lib/main.dart
@@ -1,115 +1,271 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:async';
+import 'dart:developer';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+          useMaterial3: true,
+        ),
+        home: const AllSongsPage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Playlist extends StatelessWidget {
+  const Playlist();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.grey[400],
+          ),
+        ),
+      ],
+    ));
+    ;
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+final player = AudioPlayer();
+void playsound(String url) async {
+  try {
+    player.setAudioSource(AudioSource.uri(Uri.parse(url)));
+    player.play();
+  } on Exception {
+    log("Error parsing song");
+    // TODO
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class Sample extends StatelessWidget {
+  const Sample();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+        height: 100,
+        width: 400,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[200],
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.shade500,
+                  offset: Offset(4, 4),
+                  blurRadius: 15,
+                  spreadRadius: 1),
+              BoxShadow(
+                color: Colors.white,
+                offset: Offset(-4, -4),
+                blurRadius: 5,
+              ),
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              "1",
+              style: GoogleFonts.oxygen(
+                  textStyle:
+                      TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+            ),
+            const VerticalDivider(
+              width: 20,
+              thickness: 1,
+              indent: 20,
+              endIndent: 20,
+              color: Colors.grey,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Title of sample",
+                  style: GoogleFonts.oxygen(
+                      textStyle:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                ),
+                Text(
+                  "Song category - duration",
+                  style: GoogleFonts.oxygen(
+                      textStyle:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w200)),
+                ),
+              ],
+            ),
+            MaterialButton(
+              onPressed: () {
+                playsound("ohgihiu");
+              },
+              elevation: 0.5,
+              height: 60,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100)),
+              child: Icon(
+                Icons.play_circle_fill_rounded,
+                size: 60,
+                color: Colors.cyan[900],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class AllSongsPage extends StatefulWidget {
+  const AllSongsPage();
+
+  @override
+  State<AllSongsPage> createState() => _AllSongsPageState();
+}
+
+class _AllSongsPageState extends State<AllSongsPage> {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermission();
+  }
+
+  requestPermission() async {
+    // Web platform don't support permissions methods.
+    if (!kIsWeb) {
+      bool permissionStatus = await _audioQuery.permissionsStatus();
+      if (!permissionStatus) {
+        await _audioQuery.permissionsRequest();
+      }
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        elevation: 1,
+        title: Text(
+          "Musically",
+          style: GoogleFonts.oxygen(
+              textStyle: TextStyle(fontSize: 26, fontWeight: FontWeight.w900)),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+            child: Icon(
+              Icons.notifications_outlined,
+              size: 32,
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(children: [
+          // recently played list
+          SizedBox(height: 25),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text(
+                  "P L A Y L I S T S",
+                  style: GoogleFonts.oxygen(
+                    textStyle:
+                        TextStyle(fontSize: 26, fontWeight: FontWeight.w900),
+                  ),
+                ),
+              ]),
+              SizedBox(height: 30),
+              Container(
+                height: 200,
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Playlist(),
+                    Playlist(),
+                    Playlist(),
+                    Playlist(),
+                    Playlist(),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                height: 373,
+                child: FutureBuilder<List<SongModel>>(
+                    future: _audioQuery.querySongs(
+                      sortType: null,
+                      orderType: OrderType.ASC_OR_SMALLER,
+                      uriType: UriType.EXTERNAL,
+                      ignoreCase: true,
+                    ),
+                    builder: (context, item) {
+                      if (item.data == null)
+                        return const CircularProgressIndicator();
+                      // When you try "query" without asking for [READ] or [Library] permission
+                      // the plugin will return a [Empty] list.
+                      if (item.data!.isEmpty)
+                        return const Text("Nothing found!");
+                      return ListView.builder(
+                        itemCount: item.data!.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(item.data![index].title),
+                            subtitle:
+                                Text(item.data![index].artist ?? "No Artist"),
+                            trailing: const Icon(Icons.arrow_forward_rounded),
+                            // This Widget will query/load image. Just add the id and type.
+                            // You can use/create your own widget/method using [queryArtwork].
+                            leading: QueryArtworkWidget(
+                              id: item.data![index].id,
+                              type: ArtworkType.AUDIO,
+                            ),
+                            onTap: () {
+                              playsound(item.data![index].uri.toString());
+                            },
+                          );
+                        },
+                      );
+                    }),
+              ),
+            ],
+          ),
+
+          //playlists list
+
+          //For you list
+        ]),
+      ),
     );
   }
 }
